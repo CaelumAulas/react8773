afc8374b159d26af16005ee69d08f1c00369875e
Artur Diniz Adam <artur.daz@gmail.com>
2019-10-23 13:17:19 -0300

    Arrumando problema da ordem do CSS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Home } from './pages/Home.js'
-
 import './css/reset.css'
 import './css/container.css'
 import './css/btn.css'
 import './css/icon.css'
 import './css/iconHeart.css'
 import './css/notificacao.css'
+
+import { Home } from './pages/Home.js'
 
 ReactDOM.render(
     <Home/>,
