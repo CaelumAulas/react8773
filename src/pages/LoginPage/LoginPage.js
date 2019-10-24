aa9947351ab6e5070b7e4345cab26d0222556bb6
Artur Diniz Adam <artur.daz@gmail.com>
2019-10-24 11:31:25 -0300

    Validacao inicial no login

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,42 +1,62 @@
-import React, { Component, Fragment } from 'react'
+import React, { useState, useRef, Fragment } from 'react'
 import { Cabecalho } from '../../components/Cabecalho/Cabecalho.js'
 import { Widget } from '../../components/Widget/Widget.js'
 
 import './loginPage.css'
 
-class LoginPage extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Cabecalho />
-                <div className="loginPage">
-                    <div className="container">
-                        <Widget>
-                            <h2 className="loginPage__title">Seja bem vindo!</h2>
-                            <form className="loginPage__form" action="/">
-                                <div className="loginPage__inputWrap">
-                                    <label className="loginPage__label" htmlFor="login">Login</label> 
-                                    <input className="loginPage__input" type="text" id="login" name="login"/>
-                                </div>
-                                <div className="loginPage__inputWrap">
-                                    <label className="loginPage__label" htmlFor="senha">Senha</label> 
-                                    <input className="loginPage__input" type="password" id="senha" name="senha"/>
-                                </div>
-                                {/* <div className="loginPage__errorBox">
-                                    Mensagem de erro!
-                                </div> */}
-                                <div className="loginPage__inputWrap">
-                                    <button className="loginPage__btnLogin" type="submit">
-                                        Logar
-                                    </button>
-                                </div>
-                            </form>
-                        </Widget>
-                    </div>
-                </div>
-            </Fragment>
-        )
+function LoginPage() {
+
+    const [isValido, setIsValido] = useState(true)
+
+    const $inputLogin = useRef(null)
+    const $inputSenha = useRef(null)
+
+    function onFormSubmit(evento) {
+        evento.preventDefault()
+
+        const usuario = $inputLogin.current.value
+        const senha = $inputSenha.current.value
+
+        const isValidoSubmit = usuario.length !== 0 && senha.length !== 0
+
+        setIsValido(isValidoSubmit)
     }
+
+    return (
+        <Fragment>
+            <Cabecalho />
+            <div className="loginPage">
+                <div className="container">
+                    <Widget>
+                        <h2 className="loginPage__title">Seja bem vindo!</h2>
+                        <form className="loginPage__form" action="/" onSubmit={ onFormSubmit }>
+                            <div className="loginPage__inputWrap">
+                                <label className="loginPage__label" htmlFor="login">Login</label> 
+                                <input ref={$inputLogin} className="loginPage__input" type="text" id="login" name="login"/>
+                            </div>
+                            <div className="loginPage__inputWrap">
+                                <label className="loginPage__label" htmlFor="senha">Senha</label> 
+                                <input ref={$inputSenha} className="loginPage__input" type="password" id="senha" name="senha"/>
+                            </div>
+                            {
+                                !isValido
+                                    ? <div className="loginPage__errorBox">
+                                        Senha ou usuário vazio
+                                      </div>
+                                    : ''
+                            }
+
+                            <div className="loginPage__inputWrap">
+                                <button className="loginPage__btnLogin" type="submit">
+                                    Logar
+                                </button>
+                            </div>
+                        </form>
+                    </Widget>
+                </div>
+            </div>
+        </Fragment>
+    )
 }
 
 
