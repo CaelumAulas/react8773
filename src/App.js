1061f9d8516f26b43e00be53c029fd6d77a285ec
Artur Diniz Adam <artur.daz@gmail.com>
2019-11-04 19:53:34 -0300

    Autenticacao das rotas duplicando logica de redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 
 import { Home } from './pages/Home.js'
 import { LoginPage } from './pages/LoginPage/LoginPage.js'
 
 export function App() {
-
     return (
         <Router>
             <Switch>
